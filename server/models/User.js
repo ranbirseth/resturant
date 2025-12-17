e58fb07deca3e55e7f0263c12e58d3d41d4422b5
Ranbir seth <ranbirseth7679554766@gmail.com>
2025-12-17 17:27:44 +0530

    login cradantials error fix

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      trim: true,
+      match: [/^[A-Za-z\s]+$/, "Name should contain only letters"],
     },
     mobile: {
-        type: String,
-        required: true,
-        unique: true,
-    }
-}, { timestamps: true });
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^[6-9]\d{9}$/, "Enter a valid 10-digit mobile number"],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
